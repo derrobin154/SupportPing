1e790f9cc0e0a5494c09e9426527fbcd802c5c46
derrobin154 <derrobin154@gmail.com>
2021-12-04 13:23:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Support Ping Discord Bot
+Dieser Bot hat in der Version 1.0 die Funktion eine gewisse Rolle anzupingen sobald ein User einen gewissen Channel betritt.
+
+## Erklärung der config.json
+
+* ``discord-applicationid`` Discord Application ID
+* ``discord-bottoken`` Discord Bottoken
+* ``discord-guildid`` ID des Discord Servers
+* ``discord-supportrole`` ID der Rolle die angepingt werden soll
+* ``discord-supportmuterole`` ID der Rolle die als "Support Mute" gilt
+* ``discord-supportchannelid`` ID des Channels welcher als "Support Warteraum" gilt
+* ``discord-teamchannelid`` ID des Channels in welche die Nachricht gesendet werden soll
+
+## Fehler oder Probleme
+Sollten Fehler auftreten bitte ich diese unter Issues zu melden. Sollten Probleme bei der Verwendung / Einrichtung auftreten stehe ich gerne via Discord zur Verfügung.
---
+++ index.js
@@ -0,0 +1,43 @@
+/*
+* Support Ping Bot für Discord
+* Geschrieben von: derrobin154#1337
+* Node Packages: discord.js
+* Zur Erstinbetriebnahme: "npm i" in Console eingeben, danach die Daten in der Config anpassen
+* und "node index.js" eingeben
+*/
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const config = require("./config.json");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+    
+
+try {
+    client.on("ready", () => {
+       console.log("Start OK.") 
+    });
+    
+    client.on("voiceStateUpdate", (beforeUpdate, afterUpdate) => {
+        if(afterUpdate.guild.id === config["discord-guildid"]){
+            if(afterUpdate.channelId === config["discord-supportchannelid"]){
+                if(afterUpdate.member.roles.cache.has(config["discord-supportrole"])) return;
+                if(afterUpdate.member.roles.cache.has(config["discord-supportmuterole"])) return;
+		if(afterUpdate.member.roles.cache.has(config["discord-supportrole2"])) return;
+                const Embed = new MessageEmbed()
+                    .setColor("#FF0000")
+                    .setTitle(`${afterUpdate.member.displayName} wartet im Support Channel`)
+                    .setAuthor(client.user.username, client.user.displayAvatarURL())
+                    .setDescription(`**${afterUpdate.member.displayName}** wartet im Channel **${afterUpdate.channel.name}**`)
+                    .setFooter("Supportping Bot by derrobin154#1337");
+                afterUpdate.guild.channels.cache.get(config["discord-teamchannelid"]).send({content: `<@&${config["discord-supportrole"]}>`, embeds: [Embed]}).then((message) => {
+                    console.log(`${afterUpdate.member.displayName} ging in Channel ${afterUpdate.channel.name}`);
+                }).catch((e) => {
+                    console.error(`FEHLER: ${e}`)
+                })
+            }
+        }
+    });
+
+    client.login(config["discord-bottoken"]);
+}catch (e) {
+    console.error("Fehler im ersten Try Block", e);
+}
